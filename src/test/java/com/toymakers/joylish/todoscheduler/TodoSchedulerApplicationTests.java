4fb45f1b38e5aec23a966cd3f226e6a49de4852f
LeeYeonJu <yeonzzu2@gmail.com>
2021-07-28 23:41:17 +0900

    env : create spring boot project

---
+++ src/test/java/com/toymakers/joylish/todoscheduler/TodoSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toymakers.joylish.todoscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
